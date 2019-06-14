e81dc926ab8f492dcdb377aea785af448b1e35a3
kramire <kramire@users.noreply.github.com>
2019-06-14 11:42:22 +0200

    refactor: Clearly defined error handling in Controllers.

--- controllers/stuffControllers.js
+++ controllers/stuffControllers.js
@@ -1,49 +1,57 @@
 const Stuff = require('../models/stuffModel')
 
+// Refactored to use const.
 module.exports.getAll = async (ctx, next) => {
-  let stuffs = await Stuff.find()
-  ctx.body = stuffs
-  ctx.status = 200
-  await next()
+  const stuffs = await Stuff.find();
+  ctx.body = stuffs;
+  ctx.status = 200;
 }
 
+// Refactored to use const.
+// Refactored to properly catch error, or update body.
 module.exports.create = async (ctx, next) => {
-  
-  let stuff = await new Stuff(ctx.request.body)
-  
-  try {
-    await stuff.save()
-  }
-  catch (e) {
-    ctx.status = 418
-    ctx.body = e
-  }
-  
-  await next()
-  // ctx.body = stuff
-  // ctx.status = 200
+  const stuff = await new Stuff(ctx.request.body);
+
+  await stuff.save()
+    .then(() => {
+      ctx.body = stuff;
+      ctx.status = 200;
+    })
+    .catch();
 }
 
+// Refactored to use const.
+// Refactored to return updated document in response body.
+// Refactored to properly catch error.
+// Refactored to update only passed values, if defined in schema. Avoids setting nulls.
+// Need then/catch bc callback would sometimes execute before it had result.
 module.exports.update = async (ctx, next) => {
-  let _id = ctx.params.id
-  let stuff = ctx.request.body
+  const updates = ctx.request.body;
+  const updateKeys = Object.keys(updates);
+  const setObj = {};
+
+  updateKeys.forEach(key => {
+    if (Object.keys(Stuff.schema.obj).includes(key)) setObj[key] = updates[key];
+  });
+
   await Stuff.findOneAndUpdate(
-    {_id},
-    {$set:
-      {
-        time: stuff.time,
-        picture: stuff.picture,
-        tags: stuff.tags
-      }
-    },
-    {new: true})
-  ctx.status = 204
-  await next()
+    {_id: ctx.params.id},
+    {$set: setObj},
+    {new: true}, ((err, updatedStuff) => err ? err : updatedStuff))
+    .then((res) => {
+      ctx.body = res;
+      ctx.status = 200;
+    })
+    .catch();
 }
 
+// Refactored to use const.
+// Refactored to check for OK status from remove method.
 module.exports.delete = async (ctx, next) => {
-  let _id = ctx.params.id
+  const _id = ctx.params.id;
   await Stuff.remove({_id})
-  ctx.status = 204
-  await next()
+    .then(res => {
+      if (res.ok === 1) ctx.status = 200
+    })
+    .catch();
 }
--- errorHandler.js
+++ errorHandler.js
@@ -1,20 +1,8 @@
 module.exports = async (ctx, next) => {
   try {
-    console.log('--------------TRY!!!!!!!');
     await next();
   } catch (err) {
-    console.error(err);
-    console.log('--------------CATCH!!!!!!!');
-    // ctx.body = undefined
     ctx.status = ctx.status >= 400 && ctx.status || 400;
     ctx.body = err.message;
-    // if (err.message) {
-    //   ctx.body = {
-    //     errors: [err.message]
-    //   }
-    // }
   }
 }
-
-
-// ctx.body = err.message ? {errors: [err.message]} : undefined
