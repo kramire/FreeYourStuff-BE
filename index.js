27c72b192e132303a2d4a598851b2b0c5bd382a7
dorothee <viard.dorothee@gmail.com>
2019-06-18 10:06:28 +0200

    feat: load .env.test when test running

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? './.env.test' : './.env'
+});
 // const http = require('http');
 
 const Koa = require('koa');
